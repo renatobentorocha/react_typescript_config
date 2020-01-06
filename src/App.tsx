7ec7edad5ab9c2a2674b31a5e4ecdd5a4014f13a
Renato Rocha <renatob.rocha@gmail.com>
2020-01-06 11:16:05 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+import logo from './logo.svg';
+
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
